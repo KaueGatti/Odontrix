d2da8d295bd6be777e7cbac6c39df91be2db4f22
KaueGatti <kauegattti@gmail.com>
2026-04-08 22:06:06 -0300

    config dockerfiles and docker-compose

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
 import {Outlet} from "react-router";
 
-
 function App() {
 
     return (
-        <div className="h-screen w-screen flex justify-center p-6">
+        <div className="h-screen w-screen p-0 m-0">
             <Outlet/>
         </div>
     )
